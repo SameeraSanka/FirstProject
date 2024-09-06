39c9f4105c0209ce27d5195c297a9217942716d8
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-09-06 23:06:24 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import { HomeComponent } from "./home/home.component";
 import { StudiesComponent } from "./Components/studies/studies.component";
+import { HomeComponent } from "./Components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatButtonModule, HomeComponent, StudiesComponent],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, StudiesComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FirstProject';
+  t1 = 'Australian Shepherd';
+  t2 = 'Sam Le';
 }
